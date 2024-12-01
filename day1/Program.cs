48afd7c6c10ecda557d233464a06966ac236ea9c
radumacocian <rmacocian@yahoo.com>
2024-12-01 11:49:37 +0100

    solve test 1

---
+++ day1/Program.cs
@@ -0,0 +1,22 @@
+﻿var input = File.ReadLines("input/input1.txt");
+var list1 = new List<int>();
+var list2 = new List<int>();
+
+foreach(var line in input) 
+{
+    var items = line.Split(' ');
+    list1.Add(int.Parse(items[0]));
+    list2.Add(int.Parse(items[1]));
+}
+
+list1.Sort();
+list2.Sort();
+
+var result = 0;
+
+for(var i = 0; i < list1.Count; i++) 
+{
+    result += Math.Abs(list1[i] - list2[i]);
+}
+
+Console.WriteLine(result);
