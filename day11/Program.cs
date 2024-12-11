2369d4181a53edbc49eb666dcfef078f9c3ecd4f
radumacocian <rmacocian@yahoo.com>
2024-12-11 09:01:00 +0100

    day 11 test 1

---
+++ day11/Program.cs
@@ -0,0 +1,35 @@
+﻿var input = File.ReadLines(args[0]);
+string line = new([.. input.ElementAt(0)]);
+
+List<double> stones = line.Split(' ').Select(double.Parse).ToList();
+
+for (int i = 0; i < 75; i++)
+{
+    Console.WriteLine(i);
+    List<double> newStones = [];
+    for (int s = 0; s < stones.Count; s++)
+    {
+        if (stones[s] == 0)
+        {
+            stones[s] = 1;
+            continue;
+        }
+        var str = stones[s].ToString();
+        var digits = str.Length;
+        if (digits % 2 == 0)
+        {
+            if (!double.TryParse(str[..(digits / 2)], out double newValue))
+            {
+                newValue = 0;
+            }
+            double newStone = double.Parse(str[(digits / 2)..]);
+            newStones.Add(newStone);
+            stones[s] = newValue;
+            continue;
+        }
+        stones[s] *= 2024;
+    }
+    stones.AddRange(newStones);
+}
+
+Console.WriteLine(stones.Count);
