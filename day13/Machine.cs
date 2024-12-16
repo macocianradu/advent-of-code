81729658b69b45cfde527ad00da442de71577f54
radumacocian <rmacocian@yahoo.com>
2024-12-16 13:44:19 +0100

    day 13

---
+++ day13/Machine.cs
@@ -0,0 +1,26 @@
+namespace Day13;
+
+public class Machine((int x, int y) ButtonA, (int x, int y) ButtonB, (int x, int y) prize)
+{
+    private static long Calibration = 10000000000000;
+    public int PriceA = 3;
+    public int PriceB = 1;
+    public (long x, long y) Prize = (prize.x + Calibration, prize.y + Calibration);
+
+    public long GetFewestTokens()
+    {
+        double aCoef = ButtonA.x * ButtonB.y - ButtonB.x * ButtonA.y;
+        var sol = Prize.x * ButtonB.y - Prize.y * ButtonB.x;
+
+        double a = sol / aCoef;
+        double b = (Prize.x - (ButtonA.x * a)) / ButtonB.x;
+
+        Console.WriteLine("a: " + a + " b: " + b);
+        if (Math.Floor(a) != a || Math.Floor(b) != b)
+        {
+            return long.MaxValue;
+        }
+        return (long)a * PriceA + (long)b * PriceB;
+    }
+
+}
---
+++ day13/Program.cs
@@ -0,0 +1,35 @@
+﻿using Day13;
+
+var input = File.ReadAllLines(args[0]);
+
+List<Machine> machines = [];
+for (var line = 0; line < input.Length; line++)
+{
+    var a = input[line].Split(':')[1].Split(',');
+    var ax = int.Parse(a[0].Split('+')[1]);
+    var ay = int.Parse(a[1].Split('+')[1]);
+
+    var b = input[line + 1].Split(':')[1].Split(',');
+    var bx = int.Parse(b[0].Split('+')[1]);
+    var by = int.Parse(b[1].Split('+')[1]);
+
+    var prize = input[line + 2].Split(':')[1].Split(',');
+    var prizex = int.Parse(prize[0].Split('=')[1]);
+    var prizey = int.Parse(prize[1].Split('=')[1]);
+    machines.Add(new((ax, ay), (bx, by), (prizex, prizey)));
+
+    line += 3;
+}
+
+long result = 0;
+
+machines.ForEach(m =>
+{
+    var tokens = m.GetFewestTokens();
+    if (tokens != long.MaxValue)
+    {
+        result += tokens;
+    }
+});
+
+Console.WriteLine(result);
