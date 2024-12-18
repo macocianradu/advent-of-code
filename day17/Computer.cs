df88ccca6dc01e7109be573ad548b5e024485d4b
radumacocian <rmacocian@yahoo.com>
2024-12-18 10:24:59 +0100

    day 17 test 2

--- day17/Computer.cs
+++ day17/Computer.cs
@@ -1,103 +1,96 @@
 namespace day17;
 
-public class Computer(int A, int B, int C, List<int> Instructions)
+public class Computer(ulong A, ulong B, ulong C, List<int> Instructions)
 {
     public int IP { get; set; }
-    public List<int> Output { get; set; } = [];
+    public List<ulong> Output { get; set; } = [];
 
     public bool Execute()
     {
-        //Console.Write("A: " + A);
-        //Console.Write(" B: " + B);
-        //Console.Write(" C: " + C);
         if (IP >= Instructions.Count)
         {
             return false;
         }
-        int instruction = Instructions[IP];
-        int op = Instructions[IP + 1];
-        //Console.Write(" i: " + instruction + " op: " + op + " => ");
+        uint instruction = (uint)Instructions[IP];
+        uint op = (uint)Instructions[IP + 1];
         var operation = GetOperation(instruction);
         operation()(op);
-        //Console.Write(" A: " + A);
-        //Console.Write(" B: " + B);
-        //Console.Write(" C: " + C);
         return true;
     }
 
-    private void Adv(int op)
+    private void Adv(uint op)
     {
         var deno = GetOperand(op);
-        A = Convert.ToInt32(Math.Floor(A / Math.Pow(2, deno)));
+        A = (ulong)Math.Truncate(A / Math.Pow(2, deno));
         IP += 2;
     }
 
-    private void Bxl(int op)
+    private void Bxl(uint op)
     {
         B ^= op;
         IP += 2;
     }
 
-    private void Bst(int op)
+    private void Bst(uint op)
     {
-        B = GetOperand(op) % 8;
+        B = (uint)GetOperand(op) % 8;
         IP += 2;
     }
 
-    private void Jnz(int op)
+    private void Jnz(uint op)
     {
         if (A == 0)
         {
             IP += 2;
             return;
         }
-        IP = op;
+        IP = (int)op;
     }
 
-    private void Bxc(int op)
+    private void Bxc(uint op)
     {
         B ^= C;
         IP += 2;
     }
 
-    private void Out(int op)
+    private void Out(uint op)
     {
         var x = GetOperand(op);
         Output.Add(x % 8);
         IP += 2;
     }
 
-    private void Bdv(int op)
+    private void Bdv(uint op)
     {
         var deno = GetOperand(op);
-        B = Convert.ToInt32(Math.Floor(A / Math.Pow(2, deno)));
+        B = (ulong)Math.Truncate(A / Math.Pow(2, deno));
         IP += 2;
     }
 
-    private void Cdv(int op)
+    private void Cdv(uint op)
     {
         var deno = GetOperand(op);
-        C = Convert.ToInt32(Math.Floor(A / Math.Pow(2, deno)));
+        C = (ulong)Math.Truncate(A / Math.Pow(2, deno));
         IP += 2;
     }
 
-    private Func<Action<int>> GetOperation(int opcode)
+    private Func<Action<uint>> GetOperation(uint opcode)
     {
         return opcode switch
         {
-            0 => () => (int op) => Adv(op),
-            1 => () => (int op) => Bxl(op),
-            2 => () => (int op) => Bst(op),
-            3 => () => (int op) => Jnz(op),
-            4 => () => (int op) => Bxc(op),
-            5 => () => (int op) => Out(op),
-            6 => () => (int op) => Bdv(op),
-            7 => () => (int op) => Cdv(op),
+            0 => () => (uint op) => Adv(op),
+            1 => () => (uint op) => Bxl(op),
+            2 => () => (uint op) => Bst(op),
+            3 => () => (uint op) => Jnz(op),
+            4 => () => (uint op) => Bxc(op),
+            5 => () => (uint op) => Out(op),
+            6 => () => (uint op) => Bdv(op),
+            7 => () => (uint op) => Cdv(op),
             _ => throw new Exception("Opcode doesn't exist"),
         };
     }
 
-    private int GetOperand(int op)
+    private ulong GetOperand(uint op)
     {
         if (op < 4)
         {
@@ -119,6 +112,6 @@ public class Computer(int A, int B, int C, List<int> Instructions)
         {
             throw new Exception("Invalid operand");
         }
-        return -1;
+        throw new Exception("Invalid operand");
     }
 }
--- day17/Program.cs
+++ day17/Program.cs
@@ -2,21 +2,105 @@ using day17;
 
 var input = File.ReadAllLines(args[0]);
 
-int A = int.Parse(input[0].Split(':')[1]);
-int B = int.Parse(input[1].Split(':')[1]);
-int C = int.Parse(input[2].Split(':')[1]);
+var B = uint.Parse(input[1].Split(':')[1]);
+var C = uint.Parse(input[2].Split(':')[1]);
 
 var instructions = input[4].Split(':')[1].Split(',').Select(int.Parse).ToList();
 
-var computer = new Computer(A, B, C, instructions);
-
-while (computer.Execute())
+var tasks = new List<Task>();
+Computer computer;
+List<uint> positions = [];
+var digit = 1;
+var revInstructions = new List<int>(instructions);
+revInstructions.Reverse();
+var a = 1UL;
+while (positions.Count < instructions.Count)
 {
+    var upper = (long)Math.Pow(2, (digit + 1) * 3);
+    uint pos = 0;
+    uint start = 0;
+    if (positions.Count == digit)
+    {
+        start = positions.Last() + 1;
+        positions.RemoveAt(positions.Count - 1);
+    }
+    a = GetStartingPoint(positions) + start;
+    pos = start;
+    for (pos = start; pos < 8; a++, pos++)
+    {
+        computer = new Computer(a, B, C, instructions);
+        while (computer.Execute() && computer.Output.Count < instructions.Count)
+        {
+        }
+
+        if (Compare(computer.Output, revInstructions, digit))
+        {
+            positions.Add(pos);
+            break;
+        }
+    }
+    if (positions.Count < digit)
+    {
+        digit--;
+        continue;
+    }
+    digit++;
 }
 
-for (var i = 0; i < computer.Output.Count - 1; i++)
+PrintProgram(positions);
+
+Console.WriteLine(a);
+
+static bool Compare(List<ulong> output, List<int> instructions, int digits)
 {
-    Console.Write(computer.Output[i] + ",");
+    output.Reverse();
+    for (int i = 0; i < digits; i++)
+    {
+        if ((int)output[i] != instructions[i])
+        {
+            return false;
+        }
+    }
+    return true;
 }
-Console.WriteLine(computer.Output.Last());
-Console.WriteLine();
+
+static void PrintProgram<T>(List<T> program)
+{
+    program.ForEach(e => Console.Write(e + ","));
+    Console.WriteLine();
+}
+
+bool Test(ulong a)
+{
+    var computer = new Computer(a, B, C, instructions);
+    while (computer.Execute() && computer.Output.Count < instructions.Count)
+    {
+    }
+    Console.Write(a + ": ");
+    PrintProgram(computer.Output);
+
+    for (int i = 0; i < computer.Output.Count; i++)
+    {
+        if (instructions[i] != (int)computer.Output[i])
+        {
+            return false;
+        }
+    }
+    if (instructions.Count != computer.Output.Count)
+    {
+        return false;
+    }
+
+    Console.WriteLine(a);
+    return true;
+}
+
+static ulong GetStartingPoint(List<uint> positions)
+{
+    ulong result = (ulong)Math.Pow(2, 3 * positions.Count);
+    for (int i = 0; i < positions.Count; i++)
+    {
+        result += (ulong)Math.Pow(2, 3 * (positions.Count - i)) * positions[i];
+    }
+    return result;
+}
