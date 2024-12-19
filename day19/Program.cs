befe29e992a6aed3451f7e0da01f0e75d93b6688
radumacocian <rmacocian@yahoo.com>
2024-12-19 12:23:56 +0100

    day 19

---
+++ day19/Program.cs
@@ -0,0 +1,40 @@
+﻿var input = File.ReadAllLines(args[0]);
+
+List<string> patterns = input[0].Split(',').Select(e => e.Trim()).ToList();
+List<string> designs = [];
+Dictionary<string, long> possibilities = [];
+
+for (int i = 2; i < input.Length; i++)
+{
+    designs.Add(input[i]);
+}
+
+var possible = 0L;
+foreach (var design in designs)
+{
+    possible += FitDesign(design);
+}
+
+Console.WriteLine(possible);
+
+long FitDesign(string design)
+{
+    if (possibilities.TryGetValue(design, out long value))
+    {
+        return value;
+    }
+    if (string.IsNullOrWhiteSpace(design))
+    {
+        return 1;
+    }
+    var result = 0L;
+    foreach (var pattern in patterns)
+    {
+        if (design.StartsWith(pattern))
+        {
+            result += FitDesign(new string(design.Skip(pattern.Length).ToArray()));
+        }
+    }
+    possibilities.Add(design, result);
+    return result;
+}
