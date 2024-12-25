e48d438c4144e00d9e6d67621a9e792ece02dd58
Adrian Radu Macocian <radumaco@Mac.localdomain>
2024-12-25 03:17:26 +0200

    day 24 test 2

--- day24/Program.cs
+++ day24/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using day24;
 
-var input = File.ReadAllLines("input/input1.txt");
+var input = File.ReadAllLines("input/input2.txt");
 
 Dictionary<string, bool> values = [];
 var line = 0;
@@ -46,46 +46,98 @@ for (; line < input.Length; line++)
     }
 }
 
-while (true)
+
+var xes = values.Where(x => x.Key.StartsWith('x')).ToList();
+xes.Sort((x, y) => y.Key.CompareTo(x.Key));
+var xs = BinaryToLong(xes
+    .Select(s => s.Value? "1" : "0")
+    .Aggregate((s1, s2) => s1 + s2));
+var yes = values.Where(x => x.Key.StartsWith('y')).ToList();
+yes.Sort((x, y) => y.Key.CompareTo(x.Key));
+var ys = BinaryToLong(yes
+    .Select(s => s.Value ? "1" : "0")
+    .Aggregate((s1, s2) => s1 + s2));
+var v1 = Compute(values, gates);
+Console.WriteLine(xs + " + " + ys);
+Check(v1, xs + ys);
+
+static bool Check(Dictionary<string, bool> values, long expected)
 {
-    var compute = false;
+    var toCheck = values.Where(x => x.Key.StartsWith('z')).ToList();
+    toCheck.Sort((x, y) => y.Key.CompareTo(x.Key));
 
-    foreach (var gate in gates)
+    var expectedBool = LongToBinary(expected).PadLeft(toCheck.Count, '0'); 
+    Console.WriteLine(expectedBool + " - ");
+    toCheck.ForEach(x => Console.Write(x.Value ? '1' : '0'));
+
+    List<int> s = [];
+    for (var i = 0; i < toCheck.Count; i++)
     {
-        if (values.ContainsKey(gate.Out))
+        if (expectedBool[i] != (toCheck[i].Value ? '1' : '0'))
         {
-            continue;
+            s.Add(i);
+            Console.WriteLine(toCheck[i].Key);
         }
-
-        if (!values.ContainsKey(gate.A1.Item1) || !values.ContainsKey(gate.A2.Item1))
-        {
-            continue;
-        }
-        var a1 = values[gate.A1.Item1];
-        var a2 = values[gate.A2.Item1];
-        gate.A1= (gate.A1.Item1, a1);
-        gate.A2= (gate.A2.Item1, a2);
-        values.Add(gate.Out, gate.Compute()!.Value); 
-        compute = true;
     }
 
-    if (!compute)
+    if (s.Count == 0)
     {
-        break;
+        return true;
+    }
+
+    Console.WriteLine("swap");
+    toCheck[s[0]] = new(toCheck[s[0]].Key, !toCheck[s[0]].Value);
+    toCheck[s[1]] = new(toCheck[s[1]].Key, !toCheck[s[1]].Value);
+    return false;
+}
+
+static Dictionary<string, bool> Compute(Dictionary<string, bool> values, List<Gate> gates)
+{
+    while (true)
+    {
+        var compute = false;
+
+        foreach (var gate in gates)
+        {
+            if (values.ContainsKey(gate.Out))
+            {
+                continue;
+            }
+
+            if (!values.ContainsKey(gate.A1.Item1) || !values.ContainsKey(gate.A2.Item1))
+            {
+                continue;
+            }
+
+            var a1 = values[gate.A1.Item1];
+            var a2 = values[gate.A2.Item1];
+            gate.A1 = (gate.A1.Item1, a1);
+            gate.A2 = (gate.A2.Item1, a2);
+            values.Add(gate.Out, gate.Compute()!.Value);
+            compute = true;
+        }
+
+        if (!compute)
+        {
+            return values;
+        }
     }
 }
 
-List<(string, char)> results = [];
-foreach (var value in values.Where(v => v.Key.StartsWith('z')))
-{
-    results.Add((value.Key, value.Value ? '1': '0'));   
-}
-results.Sort((x, y) => y.Item1.CompareTo(x.Item1));
 
-var binaryResult = "";
-foreach (var result in results)
+static long BinaryToLong(string binary)
 {
-    binaryResult += result.Item2;
+    return Convert.ToInt64(binary, 2);
 }
 
-Console.WriteLine(Convert.ToInt64(binaryResult, 2));
+static string LongToBinary(long value)
+{
+    return Convert.ToString(value, 2);
+}
+
+// see drawing lol
+// chv,jpj,kgj,rts,vvw,z07,z12,z26
+// rts <-> z07
+// jpj <-> z12
+// chv <-> vvw
+// kgj <-> z26
